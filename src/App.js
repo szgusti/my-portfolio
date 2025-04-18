e483c7a59515d088896f435d35a5545974d7bab6
gusti <g.szilardy@gmail.com>
2025-04-18 09:11:07 +0200

    Add Skills and Projects to portfolio

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Skills from "./pages/Skills";
 import Contact from "./pages/Contact";
 import NavBar from "./components/NavBar";
 
@@ -12,6 +14,8 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/skills" element={<Skills />} />
                 <Route path="/contact" element={<Contact />} />
             </Routes>
         </Router>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,6 +12,12 @@ function NavBar() {
                     <Link to="/about">About</Link>
                 </li>
                 <li>
+                    <Link to="/projects">Projects</Link>
+                </li>
+                <li>
+                    <Link to="/skills">Skills</Link>
+                </li>
+                <li>
                     <Link to="/contact">Contact</Link>
                 </li>
             </ul>
