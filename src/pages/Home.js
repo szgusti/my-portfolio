8e117f9a2b278e430550bb8146c24ddde21259bf
gusti <g.szilardy@gmail.com>
2025-04-27 18:00:40 +0200

    Update homepage text content in Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 function Home() {
-    return <h1>Welcome to My Portfolio</h1>;
+    return (
+        <div className="container">
+            <h1>
+                JAVA Developer <br /> with a passion of learning ...
+            </h1>
+            <p>
+                Hi! I'm <strong>[Your Name]</strong>, a software developer
+                specialized in Java and web technologies. This is a simple
+                showcase of my projects and contact information.
+            </p>
+        </div>
+    );
 }
 
 export default Home;
